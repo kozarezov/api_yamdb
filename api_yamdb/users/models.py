715c668cfe25408dcd0f4d89abd5abc682d1ac72
kozarezov <kozarezov.dd@gmail.com>
2022-09-05 16:18:35 +0300

    Change user models

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 USER = 'User'
 MODERATOR = 'Moderator'
 ADMIN = 'Admin'
@@ -9,17 +8,16 @@ ADMIN = 'Admin'
 ROLES = (
     (USER, 'Пользователь'),
     (MODERATOR, 'Модератор'),
-    (ADMIN, 'Админ'),
+    (ADMIN, 'Администратор'),
 )
 
 
 class User(AbstractUser):
-    bio = models.TextField(
-        'Биография',
-        blank=True,
-    )
-    role = models.CharField(
-        max_length=15,
-        choices=ROLES,
-        default=USER
-    )
+    """Кастомная модель User."""
+
+    username = models.CharField('Логин', max_length=150, unique=True)
+    email = models.EmailField('Email', max_length=254, unique=True)
+    first_name = models.CharField('Имя', max_length=150, blank=True)
+    last_name = models.CharField('Фамилия', max_length=150, blank=True)
+    bio = models.TextField('Информация о себе', blank=True)
+    role = models.CharField('Роль', max_length=15, choices=ROLES, default=USER)
