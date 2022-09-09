d0f1e7c26b707f8a62d77a9f5a6c24fc5e324091
Алексей Шемякин <shemyakin.geo@gmail.com>
2022-09-09 22:04:40 +0300

    Fix pytest (urls)

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -9,17 +9,10 @@ v1_router.register('users', views.UserViewSet, basename='users')
 v1_router.register('categories', views.CategoryViewSet)
 v1_router.register('genres', views.GenreViewSet)
 v1_router.register('titles', views.TitleViewSet)
-v1_router.register(r'titles/(?P<title_id>\d+)/reviews/', views.ReviewViewSet,
-                   basename='reviews')
-v1_router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/',
-                   views.ReviewViewSet,
+v1_router.register(r'titles/(?P<title_id>\d+)/reviews', views.ReviewViewSet,
                    basename='reviews')
 v1_router.register(r'titles/(?P<title_id>\d+)/reviews/('
-                   r'?P<review_id>\d+)/comments/', views.CommentViewSet,
-                   basename='comments')
-v1_router.register(r'titles/(?P<title_id>\d+)/reviews/('
-                   r'?P<review_id>\d+)/comments/('
-                   r'?P<comment_id>\d+)/', views.CommentViewSet,
+                   r'?P<review_id>\d+)/comments', views.CommentViewSet,
                    basename='comments')
 
 
