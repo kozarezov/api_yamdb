ee018f208949c559bf9acc8a9e2b981c18130f78
Pomor29 <shemyakin.geo@gmail.com>
2022-09-13 13:18:15 +0300

    Fix after IRA

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -7,8 +7,8 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny, IsAuthenticated, \
-    IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (AllowAny, IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
